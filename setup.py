ed9d770bb92f77b3a948dd879c44de410fab8422
Chris Beaven <chris@chris-studio.(none)>
2009-09-07 06:22:03 +0800

    Make the setup.py file use the README contents as it's long_description

--- setup.py
+++ setup.py
@@ -2,6 +2,12 @@ from distutils.core import setup
 import os
 
 
+README_FILE = open('README')
+try:
+    LONG_DESCRIPTION = README_FILE.read()
+finally:
+    README_FILE.close()
+
 ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
 DATA_DIR = os.path.join(ROOT_DIR, 'django_startproject', 'project_template')
 STARTPROJECT_DATA = []
@@ -10,22 +16,23 @@ for path, dirs, filenames in os.walk(DATA_DIR):
     for i, dir in enumerate(dirs):
         if dir.startswith('.'):
             del dirs[i]
-    path = path[len(DATA_DIR)+1:]
+    path = path[len(DATA_DIR) + 1:]
     STARTPROJECT_DATA.append(os.path.join('project_template', path, '*.*'))
     # Get files starting with '.' too (they are excluded from the *.* glob).
     STARTPROJECT_DATA.append(os.path.join('project_template', path, '.*'))
 
 
-setup(name = 'django-startproject',
-      version = '1.0a',
-      author = 'Lincoln Loop',
-      author_email = 'info@lincolnloop.com',
-      description = ('Create a Django project layout based on Lincoln Loop '
+setup(name='django-startproject',
+      version='1.0a',
+      author='Lincoln Loop',
+      author_email='info@lincolnloop.com',
+      description=('Create a Django project layout based on Lincoln Loop '
                      'best practices.'),
-      packages = ['django_startproject'],
+      long_description=LONG_DESCRIPTION,
+      packages=['django_startproject'],
       package_data={'django_startproject': STARTPROJECT_DATA},
-      scripts = ['bin/django-startproject.py'],
-      classifiers = [
+      scripts=['bin/django-startproject.py'],
+      classifiers=[
           'Development Status :: 3 - Alpha',
           'Environment :: Web Environment',
           'Framework :: Django',
