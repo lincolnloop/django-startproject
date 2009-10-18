c34eac198172c304d27d82d434a4413c27d6a1a1
Chris Beaven <smileychris@gmail.com>
2009-10-19 06:03:17 +0800

    Update setup.py to install manage.py as a script

--- django_startproject/project_template/setup.py
+++ django_startproject/project_template/setup.py
@@ -5,5 +5,5 @@ setup(name='myproject',
       version='0.1',
       packages=find_packages(),
       package_data={'myproject': ['bin/*.*', 'static/*.*', 'templates/*.*']},
-      exclude_package_data={'myproject': ['bin/*.pyc']}
-     )
+      exclude_package_data={'myproject': ['bin/*.pyc']},
+      scripts=['myproject/bin/manage.py'])
