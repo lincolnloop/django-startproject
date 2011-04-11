4ef1311ba7949d48c74c60444dafaa7ee68f11e7
Chris Beaven <smileychris@gmail.com>
2011-04-11 16:28:50 +1200

    Make the docs.conf template use the current year. Fixes #12

--- django_startproject/project_template/docs/conf.py
+++ django_startproject/project_template/docs/conf.py
@@ -12,6 +12,7 @@
 # serve to show the default.
 
 import sys, os
+import datetime
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -38,7 +39,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'myproject'
-copyright = u'2009, myauthor'
+copyright = u'%d, myauthor' % datetime.date.today().year
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
